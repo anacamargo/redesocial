679d2bd89c120d2f1bcb84c206569492155be924
anacamargo <anaclaudia2501@gmail.com>
2019-03-22 15:51:15 -0300

    Inclusão dos arquivos básicos

---
+++ script.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCF7-ZQ4RsPeOd73t4Iksy731mCIRHZQgo",
+    authDomain: "rede-social-teste.firebaseapp.com",
+    databaseURL: "https://rede-social-teste.firebaseio.com",
+    projectId: "rede-social-teste",
+    storageBucket: "",
+    messagingSenderId: "406251573358"
+};
+
+firebase.initializeApp(config);
