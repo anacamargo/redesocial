89a35152b5fda868264626b006a68093a3649be1
laizaraujo8 <laiz.araujo8@gmail.com>
2019-04-07 13:19:19 -0300

    timeline css v3

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -43,3 +43,63 @@ $(document).ready(function () {
         });
     });
 });
+
+let i = 0;
+ $('#incrementa').click(function(){ 
+       
+    if (i < 20 ) {
+            i++;
+        } else if (i = i++) {
+            i = 0;
+        }
+        document.getElementById("display").innerHTML = i;
+ })
+
+ $('#disminuye').click(function(){ 
+ 
+  if (i > 0) {--i;} 
+        document.getElementById("display").innerHTML = i;
+ })
+
+
+
+
+
+
+
+
+
+
+
+/**
+     * BOTÕES DO CARD
+     
+    let inner = document.createElement("div");
+    inner.classList.add('row')
+
+    // Botão adicionar
+    let button_add = document.createElement("button");
+    button_add.classList.add('btn', 'btn-link', 'col-3');
+    button_add.setAttribute('onclick', "curtir('" + id + "')");
+    button_add.innerText = 'curtir';
+    inner.appendChild(button_add);
+
+    // Contador de curtidas
+    let counter = document.createElement("span");
+    counter.innerHTML = informacao.curtidas;
+    counter.classList.add('col-3', 'text-center', 'count-number');
+    inner.appendChild(counter);
+
+    // Botão de subtrair
+    let button_sub = document.createElement("button");
+    button_sub.classList.add('btn', 'btn-link', 'col-3');
+    button_sub.setAttribute('onclick', "descurtir('" + id + "')");
+    button_sub.innerText = 'descurtir';
+    inner.appendChild(button_sub);
+
+    $('#btn-share').on('click', function () {
+        let newPost = $('#text-area').val();
+        let messageFromDB = database.ref('posts/' + USER_ID).push({
+            user: USER_ID,
+            message: newPost
+        });*/
